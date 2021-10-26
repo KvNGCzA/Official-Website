9a085c13a53581e5631cf0b34cdf4dde52fa4f4f
Ruddy Setiadi Gunawan <89178109+ru4871SG@users.noreply.github.com>
2021-10-26 14:23:00 -0500

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,41 +1,41 @@
 interface routeType {
-  name: string;
-  path: string;
-}
-
-const routes: Array<routeType> = [
-  {
-    name: "Home",
-    path: "/",
-  },
-  {
-    name: "Instant Watch",
-    path: "/instant-watch",
-  },
-  {
-    name: "Deftify Indexes",
-    path: "/deftify-indexes",
-  },
-  {
-    name: "Play n’Earn",
-    path: "/play-n-earn",
-  },
-  {
-    name: "Private  Access Pad",
-    path: "/private-access-pad",
-  },
-  {
-    name: "Staking",
-    path: "/staking",
-  },
-  {
-    name: "Team",
-    path: "/team",
-  },
-  {
-    name: "Contact Us",
-    path: "/contact-us",
-  },
-];
-
-export default routes;
+    name: string;
+    path: string;
+  }
+  
+  const routes: Array<routeType> = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Instant Watch",
+      path: "/", //path: "/instant-watch" <= supposedly, will edit when the platform is ready
+    },
+    {
+      name: "Deftify Indexes",
+      path: "/", //path: "/deftify-indexes" <= supposedly, will edit when the platform is ready
+    },
+    {
+      name: "Play n’Earn",
+      path: "/", //path: "/play-n-earn" <= supposedly, will edit when the platform is ready
+    },
+    {
+      name: "Private  Access Pad",
+      path: "/", //path: "/private-access-pad" <= supposedly, will edit when the platform is ready
+    },
+    {
+      name: "Staking",
+      path: "/", //path: ""/staking" <= supposedly, will edit later
+    },
+    {
+      name: "Team",
+      path: "/team",
+    },
+    {
+      name: "Contact Us",
+      path: "/", //path: "/contact-us" <= supposedly, will edit later
+    },
+  ];
+  
+  export default routes;
