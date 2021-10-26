e2d81bcef6d88cbb8eaa5afec5b6db3c405d7c2f
Ruddy Setiadi Gunawan <89178109+ru4871SG@users.noreply.github.com>
2021-10-26 15:11:05 -0500

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -45,11 +45,12 @@ export const Home = (props: Props) => {
           <Spacing fullWidth marginTop="50px">
             <Flex flexDirectionSm="column">
               <Spacing marginRight="40px" marginRightSm="0px" marginBottomSm="20px">
-                  <RegularButton stretch><PlayIcon style={{ marginRight: 10 }} />Watch Demo</RegularButton>
+              <OutlinedButton>
+                <PlayIcon style={{ marginRight: 10 }} />
+                Watch Demo
+              </OutlinedButton>
               </Spacing>
-              <a href="https://docsend.com/view/pkxf3t4dy9bjyjt6" target="_blank" rel="noreferrer"><OutlinedButton>
-                Whitepaper
-              </OutlinedButton></a>
+              <a href="https://docsend.com/view/pkxf3t4dy9bjyjt6" target="_blank" rel="noreferrer"><RegularButton stretch>Whitepaper</RegularButton></a>
             </Flex>
 
               <Spacing fullWidth marginTop="20px">
