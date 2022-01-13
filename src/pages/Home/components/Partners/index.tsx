8bf347a91eae871803a11a5736ed1a50f8bdde65
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-13 16:26:28 -0500

    test

--- src/pages/Home/components/Partners/index.tsx
+++ src/pages/Home/components/Partners/index.tsx
@@ -46,19 +46,19 @@ export const Partners = () => {
 			&nbsp;
 			<p className="section-text__title--small small">Deftify On The Media</p>
 			&nbsp;
-			<div class="column" style="background-color:#aaa;">
+			<div className="column">
     <h2>Column 1</h2>
     <p>Some text..</p>
   </div>
-  <div class="column" style="background-color:#bbb;">
+  <div className="column" style="background-color:#bbb;">
     <h2>Column 2</h2>
     <p>Some text..</p>
   </div>
-  <div class="column" style="background-color:#ccc;">
+  <div className="column" style="background-color:#ccc;">
     <h2>Column 3</h2>
     <p>Some text..</p>
   </div>
-  <div class="column" style="background-color:#ddd;">
+  <div className="column" style="background-color:#ddd;">
     <h2>Column 4</h2>
     <p>Some text..</p>
   </div>
