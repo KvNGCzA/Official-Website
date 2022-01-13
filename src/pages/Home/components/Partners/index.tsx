354ff8de0272cfef7bb4acae9f539fa6502c3d7e
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-13 15:29:34 -0500

    update

--- src/pages/Home/components/Partners/index.tsx
+++ src/pages/Home/components/Partners/index.tsx
@@ -19,19 +19,6 @@ const PARTNERS: {
 	{image: 'averagemenvc-svg.svg', key: uniqueRef()}
 ];
 
-const MEDIA: {
-	image: string;
-	key: string;
-}[] = [
-	{image: 'lead-wallet.svg', key: uniqueRef()},
-	{image: 'unvest.svg', key: uniqueRef()},
-	{image: 'gotbit-svg.svg', key: uniqueRef()},
-	{image: 'polygon-partner.svg', key: uniqueRef()},
-	{image: 'despace.svg', key: uniqueRef()},
-	{image: 'dec-ventures.svg', key: uniqueRef()},
-	{image: 'oasis-capital.svg', key: uniqueRef()},
-	{image: 'averagemenvc-svg.svg', key: uniqueRef()}
-];
 
 export const Partners = () => {
 	return (
@@ -59,17 +46,6 @@ export const Partners = () => {
 			&nbsp;
 			<p className="section-text__title--small small">Deftify On The Media</p>
 			&nbsp;
-			<div className="partner-cont">
-				{PMEDIA.map(partner => <div
-					className="partner"
-					key={partner.key}
-					style={{
-						background:     `url('./images/${partner.image}') no-repeat center center`,
-						backgroundSize: '69% auto'
-					}}
-				>
-				</div>)}
-			</div>
 		</Spacing>
 	);
 };
